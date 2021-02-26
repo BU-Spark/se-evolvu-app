3f90bb11a6a03f6caeafb0c364e7a05dc82b54e0
tintheturtle <tinhoang@bu.edu>
2021-02-25 21:49:05 -0500

    created homepage and errorpage

---
+++ src/components/ErrorPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    
+    return (
+        <div>
+            The page you are looking for cannot be found. 
+        </div>
+    )
+}
+
+export default ErrorPage;
---
+++ src/components/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+
+    return (
+        <div>
+            This is the temporary homepage.
+        </div>
+    )
+}
+
+export default Homepage;
