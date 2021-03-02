af24624a6c9c1957b71f403ff89721c1d6b5c830
tintheturtle <tinhoang@bu.edu>
2021-03-02 15:15:07 -0500

    started on navbar, missing logo in center

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import "./index.css"
+
+const Navbar = () => {
+
+    return (
+        <div id="nav-wrapper">
+            <div id="navbar">
+                <ul className="navbar-linklist">
+                    <li className="navbar-link" >
+                        <Link className="navitem" to="/"> Find a Coach </Link>
+                    </li>
+                    <li className="navbar-link" >
+                        <Link className="navitem" to="/home"> Apply as Coach </Link>
+                    </li>
+                </ul>
+                
+                <ul className="navbar-linklist">
+                    <li className="navbar-link" >
+                        <Link className="navitem" to="/about"> Sign In </Link>
+                    </li>
+                    <li className="navbar-link" >
+                        <Link className="navitem" to="/about"> Sign Up </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+
+}
+
+export default Navbar;
