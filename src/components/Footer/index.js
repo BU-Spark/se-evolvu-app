2cfcc684dcccaa4272b10e2cfb484d5527f02b42
tintheturtle <tinhoang@bu.edu>
2021-03-03 16:54:26 -0500

    removed unnecessary files (i.e. 0, x, { ) and removed unused imports in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import {   Link,
-    BrowserRouter,
-    Switch,
-    Route } from 'react-router-dom';
-import styles from "./index.css";
+import {   Link } from 'react-router-dom';
+import "./index.css";
 
 const Footer = () => {
 
