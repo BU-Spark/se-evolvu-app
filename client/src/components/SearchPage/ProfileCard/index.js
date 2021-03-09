b550686738c66f354085c2bb843dc0ae837d5a22
tintheturtle <tinhoang@bu.edu>
2021-03-08 20:01:19 -0500

    added buttons for format preference and view preference

---
+++ client/src/components/SearchPage/ProfileCard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfileCard = () => {
+
+    return (
+        <div>
+            Coach information
+        </div>
+    )
+}
+
+export default ProfileCard
--- client/src/components/SearchPage/index.js
+++ client/src/components/SearchPage/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+
 const SearchPage = (props) => {
     return (
-        <div>
-            {props.location.state.focus}
-            {props.location.state.local}
+        <div className="search-results-body container-fluid">
+            <p style={{padding: "1rem", zIndex: "-1", background:""}}>
+                You are searching for {props.location.state.focus} in {props.location.state.local}
+            </p>
+            <Row>
+                <Col sm={3}>
+                    Filter
+                </Col>
+                <Col sm={6}>
+                    <div>
+                        <ButtonToolbar
+                            className="justify-content-between"
+                            aria-label="Toolbar with Button groups"
+                        >
+                            <ButtonGroup aria-label="First group">
+                                <Button variant="outline-secondary" style={{margin: "0.5rem", borderRadius: "0.25rem"}}>In-Person</Button>{' '}
+                                <Button variant="outline-secondary" style={{margin: "0.5rem", borderRadius: "0.25rem"}}>Remote</Button>{' '}
+                            </ButtonGroup>
+                            <ButtonGroup aria-label="Second group">
+                                <Button variant="outline-secondary" style={{margin: "0.5rem", borderRadius: "0.25rem"}}>List</Button>{' '}
+                                <Button variant="outline-secondary" style={{margin: "0.5rem", borderRadius: "0.25rem"}}>Grid</Button>{' '}
+                            </ButtonGroup>
+                        </ButtonToolbar>
+                    </div>
+                    <div>
+                        Coach 1
+                    </div>
+                    <div>
+                        Coach 1
+                    </div>
+                </Col>
+                <Col sm={3}>
+                    Sort
+                </Col>
+            </Row>
         </div>
     )
 }
