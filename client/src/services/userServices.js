47e89f9bb56101af6b203c283a126427dc38cc13
tintheturtle <tinhoang@bu.edu>
2021-03-20 10:20:37 -0400

    made authentiation and example services with authentication headers

---
+++ client/src/services/userServices.js
@@ -0,0 +1,16 @@
+// Code made from example from https://bezkoder.com/react-hooks-redux-login-registration-example/
+
+import axios from "axios";
+import authHeader from './authHeader.js';
+
+const API_URL = "http:localhost:8000";
+
+const getTestItems = () => {
+    return axios.get(API_URL + '/test', { headers: authHeader() });
+};
+
+const userServices = {
+    getTestItems
+};
+
+export default userServices
