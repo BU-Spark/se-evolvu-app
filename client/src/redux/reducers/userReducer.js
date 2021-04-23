e02b790325302a4def4c08ec1cd0c75be8d734b9
tintheturtle <tinhoang@bu.edu>
2021-04-22 23:41:14 -0400

    spread user reducer into individual fields instead of storing an object in redux

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,7 +1,17 @@
 import { Types } from "../actions/actionTypes"
 
 const initialState = {
-    user: null
+    user: null,
+    email: "",
+    first_name: "",
+    last_name: "",
+    active: false,
+    admin: false,
+    coach: false,
+    customer: false,
+    staff: false,
+    superuser: false,
+    last_login:"", 
 }
 
 export const userReducer = (state = initialState, action) => {
@@ -9,12 +19,34 @@ export const userReducer = (state = initialState, action) => {
         case Types.SET_USER: 
             return {
                 ...state,
-                user: action.payload.user.data.user
+                email: action.payload.data.user.email,
+                first_name: action.payload.data.user.first_name,
+                last_name: action.payload.data.user.last_name,
+                active: action.payload.data.user.is_active,
+                admin: action.payload.data.user.is_admin,
+                coach: action.payload.data.user.is_coach, 
+                customer: action.payload.data.user.is_customer, 
+                staff: action.payload.data.user.is_staff, 
+                superuser: action.payload.data.user.is_superuser,
+                last_login: action.payload.data.user.last_login, 
+            }
+        case Types.UNSET_USER: 
+            return {
+                ...state,
+                email: "",
+                first_name: "",
+                last_name: "",
+                active: false,
+                admin: false,
+                coach: false,
+                customer: false,
+                staff: false,
+                superuser: false,
+                last_login:"", 
             }
         default:
             return {
                 ...state,
-                user: null
             }
     }
 }
