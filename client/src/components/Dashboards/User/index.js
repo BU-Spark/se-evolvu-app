8fe8b7b828a2b796a72b97cdb6ee7a606d1b0405
tintheturtle <tinhoang@bu.edu>
2021-04-26 17:18:48 -0400

    conditional render for coaches and users dashboard and modified initial dummy data

--- client/src/components/Dashboards/User/index.js
+++ client/src/components/Dashboards/User/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import './index.css';
 
 const UserDashboard = () => {
 
     const name = useSelector(state => state.userReducer.first_name);
+    const coach = useSelector(state => state.userReducer.coach);
+    if (coach) {
+        return <Redirect to="/coach/dashboard"/>
+    }
 
     return (
         <div style={{ margin: '1rem'}}>
