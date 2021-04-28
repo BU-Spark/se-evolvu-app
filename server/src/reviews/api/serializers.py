99a3fa3b7c6a4a5f9f4f77974457b3608fc84609
tintheturtle <tinhoang@bu.edu>
2021-04-28 10:50:43 -0400

    modified reviews endpoint, receives a slug and token for retrieving primary keys for coaches and users

--- server/src/reviews/api/serializers.py
+++ server/src/reviews/api/serializers.py
@@ -5,4 +5,15 @@ from reviews.models import Review
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ('coach', 'reviewer', 'rating', 'body', 'date')
+        fields = ['coach', 'reviewer', 'rating', 'body', 'date']
+
+    def save(self):
+        review = Review(
+            coach = self.validated_data['coach'],
+            reviewer = self.validated_data['reviewer'],
+            rating = self.validated_data['rating'],
+            body = self.validated_data['body'],
+        )
+
+        review.save()
+        return review
--- server/src/reviews/api/views.py
+++ server/src/reviews/api/views.py
@@ -9,8 +9,49 @@ from rest_framework.generics import ListAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.authtoken.models import Token
 from reviews.models import Review
+from coaches.models import Coach
+from accounts.models import Account
+
 from reviews.api.serializers import ReviewSerializer
+
+
+@api_view(['POST', ])
+def create_coach_review(request, slug):
+    
+    # Fetch Coach Profile (only fetches their pk)
+    try:
+        coach_profile = Coach.objects.values_list('id').get(coach__slug=slug)
+    except Coach.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # Fetch Reviewer's Account (only fetches their pk)
+    try:
+        token = request.data['reviewer']['token']
+        user_id = Token.objects.get(key=token).user_id
+        user = Account.objects.values_list('id').get(id=user_id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    review_data = {
+        'coach': coach_profile[0],
+        'reviewer': user[0],
+        'rating': request.data['rating'],
+        'body': request.data['body'],
+    }
+
+    # Validate data
+    serializer = ReviewSerializer(data=review_data)
+    if serializer.is_valid():
+        # Create Review if all fields are valid
+        serializer.save()
+        return Response(status=status.HTTP_200_OK, message="success")
+    else:
+        data = serializer.errors
+        return Response(data)
+
+
+    
 
 class ReviewView(viewsets.ModelViewSet):
     queryset = Review.objects.all()
