3a69cdaddb3fca20cc3f6a2ad4516c9879bde0d0
tintheturtle <tinhoang@bu.edu>
2021-04-29 14:19:11 -0400

    switched db from sqlite to postgresql

--- server/src/evolvu/settings.py
+++ server/src/evolvu/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'evolvu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'db',
+        'NAME': 'evolvu_dev',
+        'USER': 'admin',
+        'PASSWORD': 'rpDEvGQxZtC5@',
+        'PORT': 5432
     }
 }
 
